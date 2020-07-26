0811e2413ddc3ef13ea3c6e89cc3452184def965
raniless <raniless@naver.com>
2020-07-26 23:46:55 +0900

    Two Sum II 풀이

---
+++ src/question/leetcode/easy/LP167.java
@@ -0,0 +1,30 @@
+package question.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Two Sum II - Input array is sorted
+public class LP167 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2, 7, 11, 15};
+        int target = 9;
+        LP167 lp167 = new LP167();
+        int[] result = lp167.twoSum(numbers, target);
+        for(int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<numbers.length; i++) {
+            int mod = target - numbers[i];
+            if(map.containsKey(mod)) {
+                return new int[]{map.get(mod)+1, i+1};
+            }
+            map.put(numbers[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
