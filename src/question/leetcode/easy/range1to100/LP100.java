f4c906f068c86f0f0f3b51218d20a5dbbc50e0e9
raniless <raniless@naver.com>
2020-08-24 23:46:25 +0900

    Leet Code 풀이

---
+++ src/question/leetcode/easy/range1to100/LP100.java
@@ -0,0 +1,83 @@
+package question.leetcode.easy.range1to100;
+
+import question.leetcode.util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LP100 {
+    public static void main(String[] args) {
+//        String[] a = new String[]{"1", "2'"};
+//        String[] b = new String[]{"1", null, "2'"};
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null ^ q == null) {
+            return false;
+        }
+
+        if(p != null) {
+            if(p.val != q.val) {
+                return false;
+            }
+
+            Queue<TreeNode> q1 = new LinkedList<>();
+            Queue<TreeNode> q2 = new LinkedList<>();
+
+            q1.offer(p);
+            q2.offer(q);
+            while(!q1.isEmpty() || !q2.isEmpty()) {
+                TreeNode a = q1.poll();
+                TreeNode b = q2.poll();
+
+                if(a.left == null ^ b.left == null) {
+                    return false;
+                }
+
+                if(a.left != null) {
+                    if(a.left.val != b.left.val) {
+                        return false;
+                    }
+                    else {
+                        q1.offer(a.left);
+                        q2.offer(b.left);
+                    }
+                }
+
+                if(a.right == null ^ b.right == null) {
+                    return false;
+                }
+
+                if(a.right != null) {
+                    if(a.right.val != b.right.val) {
+                        return false;
+                    }
+                    else {
+                        q1.offer(a.right);
+                        q2.offer(b.right);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /* Simple한 정답
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+
+        if(p == null || q == null) {
+            return false;
+        }
+
+        if(p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    */
+}
