8588cbf760de0723b3a061a127c1aeebb0b5853e
raniless <raniless@naver.com>
2020-08-30 00:03:45 +0900

    LeetCode 풀이

---
+++ src/question/leetcode/easy/range101to200/LP107.java
@@ -0,0 +1,44 @@
+package question.leetcode.easy.range101to200;
+
+import question.leetcode.util.LeetCodeUtil;
+import question.leetcode.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Binary Tree Level Order Traversal II
+public class LP107 {
+    public static void main(String[] args) {
+        Integer[] input = new Integer[]{3,9,20,null,null,15,7};
+        TreeNode root = LeetCodeUtil.makeTreeNode(input);
+
+        LP107 lp107 = new LP107();
+        List<List<Integer>> result = lp107.levelOrderBottom(root);
+        System.out.println(result);
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrderList = new ArrayList<>();
+
+        addLevelOrderList(levelOrderList, root, 1);
+        return levelOrderList;
+    }
+
+    private void addLevelOrderList(List<List<Integer>> levelOrderList, TreeNode node, int level) {
+        if(node == null) {
+            return;
+        }
+
+        if(levelOrderList.size() >= level) {
+            levelOrderList.get(levelOrderList.size()-level).add(node.val);
+        }
+        else {
+            List<Integer> list = new ArrayList<>();
+            list.add(node.val);
+            levelOrderList.add(0, list);
+        }
+
+        addLevelOrderList(levelOrderList, node.left, level+1);
+        addLevelOrderList(levelOrderList, node.right, level+1);
+    }
+}
---
+++ src/question/leetcode/easy/range101to200/LP108.java
@@ -0,0 +1,32 @@
+package question.leetcode.easy.range101to200;
+
+import question.leetcode.util.LeetCodeUtil;
+import question.leetcode.util.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+// Convert Sorted Array to Binary Search Tree
+public class LP108 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-10,-3,0,5,9};
+
+        LP108 lp108 = new LP108();
+        TreeNode result = lp108.sortedArrayToBST(nums);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return createBST(nums, 0, nums.length-1);
+    }
+
+    private TreeNode createBST(int[] nums, int left, int right) {
+        if(left > right) {
+            return null;
+        }
+
+        int mid = (left+right) / 2 ;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = createBST(nums, left, mid-1);
+        root.right = createBST(nums, mid+1, right);
+
+        return root;
+    }
+}
