ca16ca5bcb575bdeb5c60114ae69ebe0a8a5391a
raniless <raniless@naver.com>
2020-09-01 00:00:19 +0900

    Leet Code 풀이

--- src/question/leetcode/easy/range101to200/LP110.java
+++ src/question/leetcode/easy/range101to200/LP110.java
@@ -3,13 +3,16 @@ package question.leetcode.easy.range101to200;
 import question.leetcode.util.LeetCodeUtil;
 import question.leetcode.util.TreeNode;
 
+// Balanced Binary Tree
 public class LP110 {
     public static void main(String[] args) {
         Integer[] input = new Integer[]{3,9,20,null,null,15,7};
+//        Integer[] input = new Integer[]{1,2,2,3,3,null,null,4,4};
         TreeNode root = LeetCodeUtil.makeTreeNode(input);
 
         LP110 lp110 = new LP110();
         boolean result = lp110.isBalanced(root);
+        System.out.println(result);
     }
 
     public boolean isBalanced(TreeNode root) {
