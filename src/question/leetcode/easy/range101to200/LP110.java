1d30a7a69fe446535d12bd031a87a616051dbca1
raniless <raniless@naver.com>
2020-08-31 23:59:06 +0900

    Leet Code 풀이

---
+++ src/question/leetcode/easy/range101to200/LP110.java
@@ -0,0 +1,35 @@
+package question.leetcode.easy.range101to200;
+
+import question.leetcode.util.LeetCodeUtil;
+import question.leetcode.util.TreeNode;
+
+public class LP110 {
+    public static void main(String[] args) {
+        Integer[] input = new Integer[]{3,9,20,null,null,15,7};
+        TreeNode root = LeetCodeUtil.makeTreeNode(input);
+
+        LP110 lp110 = new LP110();
+        boolean result = lp110.isBalanced(root);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+
+        int leftH = getHeight(node.left);
+        int rightH = getHeight(node.right);
+        int diff = Math.abs(leftH - rightH);
+
+        //-1 : Unbalanced Tree
+        if(leftH == -1 || rightH == -1 || diff > 1) {
+            return -1;
+        }
+
+        return Math.max(leftH, rightH) + 1;
+    }
+}
