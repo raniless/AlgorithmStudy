06a1d2c0b28cfa408a7ae8c5e4052b86d358b7b0
raniless <raniless@naver.com>
2020-09-04 00:02:10 +0900

    Leet Code 풀이

---
+++ src/question/leetcode/easy/range101to200/LP111.java
@@ -0,0 +1,43 @@
+package question.leetcode.easy.range101to200;
+
+import question.leetcode.util.LeetCodeUtil;
+import question.leetcode.util.TreeNode;
+
+// Minimum Depth of Binary Tree
+public class LP111 {
+    public static void main(String[] args) {
+//        Integer[] input = new Integer[]{3,9,20,null,null,15,7};
+        Integer[] input = new Integer[]{1,2};
+        TreeNode root = LeetCodeUtil.makeTreeNode(input);
+
+        LP111 lp111 = new LP111();
+        int result = lp111.minDepth(root);
+        System.out.println(result);
+    }
+
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        return minDepthOfTree(root);
+    }
+
+    private int minDepthOfTree(TreeNode node) {
+        if(node.left == null && node.right == null) {
+            return 1;
+        }
+
+        if(node.left == null) {
+            return minDepthOfTree(node.right) + 1;
+        }
+        else if(node.right == null) {
+            return minDepthOfTree(node.left) + 1;
+        }
+
+        int left = minDepthOfTree(node.left);
+        int right = minDepthOfTree(node.right);
+
+        return Math.min(left, right) + 1;
+    }
+}
---
+++ src/question/leetcode/easy/range101to200/LP112.java
@@ -0,0 +1,63 @@
+package question.leetcode.easy.range101to200;
+
+import question.leetcode.util.LeetCodeUtil;
+import question.leetcode.util.TreeNode;
+
+public class LP112 {
+    public static void main(String[] args) {
+//        Integer[] input = new Integer[]{5,4,8,11,null,13,4,7,2,null,null,null,1};
+        Integer[] input = new Integer[]{};
+        TreeNode root = LeetCodeUtil.makeTreeNode(input);
+        int sum = 22;
+
+        LP112 lp112 = new LP112();
+        boolean result = lp112.hasPathSum(root, sum);
+        System.out.println(result);
+    }
+
+    /*
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+
+        return hasPathSum(root, sum, 0);
+    }
+
+    private boolean hasPathSum(TreeNode node, int sum, int subSum) {
+        if (node.left == null && node.right == null) {
+            return subSum + node.val == sum;
+        }
+
+        if (node.left == null) {
+            return hasPathSum(node.right, sum, subSum + node.val);
+        }
+        if (node.right == null) {
+            return hasPathSum(node.left, sum, subSum + node.val);
+        }
+
+        if (hasPathSum(node.left, sum, subSum + node.val)) {
+            return true;
+        }
+
+        if (hasPathSum(node.right, sum, subSum + node.val)) {
+            return true;
+        }
+
+        return false;
+    }
+    */
+
+    //훨씬 간결한 로직
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.left == null && root.right == null) {
+            return (sum - root.val) == 0;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
