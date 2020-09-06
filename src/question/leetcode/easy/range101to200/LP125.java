b223e1903eb21196671547e237a6fb9aa64fb633
raniless <raniless@naver.com>
2020-09-06 21:35:33 +0900

    LeetCode 풀이

---
+++ src/question/leetcode/easy/range101to200/LP125.java
@@ -0,0 +1,44 @@
+package question.leetcode.easy.range101to200;
+
+// Valid Palindrome
+public class LP125 {
+    public static void main(String[] args) {
+//        String s = "A man, a plan, a canal: Panama";
+//        String s = "race a car";
+//        String s = ".,";
+        String s = "0P";
+        LP125 lp125 = new LP125();
+        boolean result = lp125.isPalindrome(s);
+        System.out.println(result);
+    }
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if(!isAlphaNumeric(s.charAt(left))) {
+                left++;
+                continue;
+            }
+
+            if(!isAlphaNumeric(s.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNumeric(Character ch) {
+        return Character.isAlphabetic(ch) || Character.isDigit(ch);
+    }
+}
