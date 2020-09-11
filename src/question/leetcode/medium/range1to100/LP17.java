e639f11f0c91003b073a9b1cc1552a65a0ee7ee7
raniless <raniless@naver.com>
2020-09-12 00:21:22 +0900

    Leet Code 풀이

---
+++ src/question/leetcode/medium/range1to100/LP17.java
@@ -0,0 +1,51 @@
+package question.leetcode.medium.range1to100;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LP17 {
+    private Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    public static void main(String[] args) {
+        String digits = "234";
+
+        LP17 lp17 = new LP17();
+        List<String> result = lp17.letterCombinations(digits);
+        System.out.println(result);
+
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> answer = new ArrayList<>();
+
+        if(digits.length() > 0) {
+            letterCombinations("", digits, 0, answer);
+        }
+
+        return answer;
+    }
+
+    private void letterCombinations(String combination, String digits, int idx, List<String> answer) {
+        if(idx == digits.length()) {
+            answer.add(combination);
+        }
+        else {
+            String digit = Character.toString(digits.charAt(idx));
+            String letter = phone.get(digit);
+            for(int i=0; i<letter.length(); i++) {
+                letterCombinations(combination+Character.toString(letter.charAt(i)), digits, idx+1, answer);
+            }
+        }
+    }
+}
