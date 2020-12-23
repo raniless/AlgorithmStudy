1bb4d295f0e1da327aaea4619f8e8a6c2a609c72
raniless <raniless@naver.com>
2020-12-24 00:08:16 +0900

    TreeNode 생성 및 출력 로직 수정

--- src/question/leetcode/util/LeetCodeUtil.java
+++ src/question/leetcode/util/LeetCodeUtil.java
@@ -1,6 +1,7 @@
 package question.leetcode.util;
 
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.Queue;
 
 public abstract class LeetCodeUtil {
@@ -38,24 +39,92 @@ public abstract class LeetCodeUtil {
         q.offer(root);
         while(idx < n) {
             TreeNode treeNode = q.poll();
+
             if(idx < n) {
                 Integer value = values[idx++];
+                TreeNode leftNode = null;
                 if(value != null) {
-                    TreeNode leftNode = new TreeNode(value);
+                    leftNode = new TreeNode(value);
                     treeNode.left = leftNode;
-                    q.offer(leftNode);
                 }
+
+                q.offer(leftNode);
             }
+
             if(idx < n) {
                 Integer value = values[idx++];
+                TreeNode rightNode = null;
                 if(value != null) {
-                    TreeNode rightNode = new TreeNode(value);
+                    rightNode = new TreeNode(value);
                     treeNode.right = rightNode;
-                    q.offer(rightNode);
                 }
+
+                q.offer(rightNode);
             }
         }
 
         return root;
+    }
+
+    public static void printTreeNode(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        int maxDepth = getMaxDepth(root, 1);
+        TreeNodeWrapper rootWrapper = new TreeNodeWrapper(1, root);
+        int currDepth = 1;
+
+        Queue<TreeNodeWrapper> queue = new LinkedList();
+        queue.offer(rootWrapper);
+        while(!queue.isEmpty()){
+            TreeNodeWrapper nodeWrapper = queue.poll();
+            TreeNode node = nodeWrapper.getNode();
+            int nodeDepth = nodeWrapper.getDepth();
+
+            if(currDepth != nodeDepth) {
+                currDepth = nodeDepth;
+                System.out.println();
+            }
+
+            System.out.print((node == null ? "null" : node.val) + "\t");
+
+            if(nodeDepth < maxDepth) {
+                if(node == null) {
+                    queue.offer(new TreeNodeWrapper(nodeDepth+1, null));
+                    queue.offer(new TreeNodeWrapper(nodeDepth+1, null));
+                }
+                else {
+                    queue.offer(new TreeNodeWrapper(nodeDepth+1, node.left));
+                    queue.offer(new TreeNodeWrapper(nodeDepth+1, node.right));
+                }
+            }
+        }
+    }
+
+    private static int getMaxDepth(TreeNode node, int depth) {
+        if(node == null) {
+            return depth-1;
+        }
+
+        return Math.max(getMaxDepth(node.left, depth+1), getMaxDepth(node.right, depth+1));
+    }
+
+    static class TreeNodeWrapper {
+        private int depth;
+        private TreeNode node;
+
+        public TreeNodeWrapper(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+
+        public int getDepth() {
+            return depth;
+        }
+
+        public TreeNode getNode() {
+            return node;
+        }
     }
 }
